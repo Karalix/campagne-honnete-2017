ed11596af85774f8b5499625f4d767b1834a5400
Ducros Alix <ducrosalix@hotmail.fr>
2017-03-19 22:23:21 +0100

    First commit

---
+++ truther.js
@@ -0,0 +1,242 @@
+var fillons = [
+    "Fillon l'escroc", 
+    "cette racaille de Fillon",
+    "Fillon, l'homme aux gros sourcils",
+    "Fillon la vermine",
+    "François le voleur",
+    "Fillon le fion", 
+    "François Fillon le voyou", 
+    "Fillon l'enfoiré", 
+    "cette enflure de François Fillon", 
+    "François Fillon (le mec qui finira en prison)", 
+    "Fillon le sale voleur", 
+    "ce FDP de Fillon", 
+    "Fillon le ripou",
+    "François Fillon cette raclure", 
+    "ce bandit de Fillon", 
+    "ce mafieu de Fillon",
+    "François le malhonnête",
+    'François Fillon (celui qui vole l\'argent public'
+];
+
+var poutous = [
+    'ce réac de Philippe Poutou',
+    'Philippe Poutou, qui a échoué au bac pro',
+    'cet énervé de Philippe Poutou',
+    'Philippe Poutou qui de toutes façons ne sera pas au deuxième tour'
+];
+
+var melenchons = [
+    'Mélenchon le démagogue',
+    'cette grande gueule de Mélenchon',
+    'le bourgeois communiste Jean Luc Mélenchon',
+    'Mélenchon le coco',
+    'ce gueulard de Mélenchon',
+    'ce vieil aigri qu\'est Mélenchon',
+    "le fantôme du parti communiste Jean-Luc Mélenchon",
+    "ce vieux râleur de Mélenchon",
+    'la Méluche'
+];
+
+
+var macrons = [
+    'Macron le ripou',
+    'Macron l\'homme à cougars',
+    'ce voleur d\'Emmanuel Macron',
+    'ce gamin d\'Emmanuel Macron',
+    'Macron l\'escroc',
+    "cet enfoiré de Macron",
+    "Macron le voleur",
+    "Macron le roublard",
+    "Macron l'enfoiré", 
+    "Macron le brigand",
+    "Macron la crapule",
+    "cette enflure de Macron",
+    "ce filou de Macron",
+    "Macron la tête à claques",
+    "Macron le cocaïnomane",
+    'ce filou d\'Emmanuel Macron'
+];
+
+var asselineaux = [
+    'cette victime de François Asselineau',
+    'Asselineau le parano',
+    'le candidat de Ligue 2, François Asselineau'
+];
+
+var aignans = [
+    'Nicolas Dupont-Aignan qui ne sera pas élu',
+    'Nicolas Dupont-Aignan (celui qui n\'aime pas TF1)',
+    'ce râleur de Dupont-Aignan'
+];
+
+var lassalles = [
+    'Jean Lassalle (le pote à Bachar)',
+    'Lassalle le randonneur',
+    'Jean Lassalle qui ne sera pas au deuxième tour'
+];
+
+var cheminades = [
+    'Jacques Cheminade (celui qui veut aller dans l\'espace)',
+    'Cheminade le chef des colons martiens',
+    'Jacques Cheminade que personne ne comprend',
+    'Jacques le fan de SF',
+    'Cheminade (qui ne passera pas le premier tour)'
+];
+
+var hamons = [
+    'Hamon, le gars complètement à l\'ouest',
+    'Benoît Hamon, qui s\'arrêtera au premier tour',
+    'Hamon le mec qui sert à rien',
+    'Hamon le fumeur de spliff',
+    'le petit Benoît Hamon',
+    'Benoît \'Ganja\' Hamon',
+    'ce loser de Hamon',
+    'ce niais de Benoît Hamon',
+    "Benoît Hamon, le futur éliminé",
+    "Benoît Hamon le brasseur de vent ",
+    "Hamon le rastafaraï",
+    "Benoît Hamon, qui ne sera pas au second tour", 
+    "ce consternant Benoît Hamon",
+    "le malheureux perdant Benoît Hamon"
+];
+
+var pens = [
+    'Marine Le Pen (la fille du borgne)',
+    'Marine Le Pen (qui a pas un peu grossi ?)',
+    'Marine la raciste',
+    'Marine la teubé',
+    'cette ordure de Marine Le Pen',
+    'Marine la facho',
+    "Le Pen la facho",
+    "Marine la morue",
+    "la grosse Le Pen", 
+    "Marine la blondasse",
+    "cette abrutie de Marine Le Pen",
+    "Marine la chieuse",
+    "Marine Le Pen la vendeuse de poisson",
+    "Marine la blondinette",
+    "Marine, la fille de cet abruti de Jean-Marie",
+    'Marine Le Pen (celle qui a peur des noirs)'
+];
+[];
+
+var arthauds = [
+    'Nathalie Marteau et Faucille',
+    'Arthaud la coco',
+    'Nathalie Arthaud qui ne passera pas le premier tour',
+    'Nathalie Arthaud qui avait fait 0.56% en 2012'
+];
+
+var hollandes = [
+    'cette loque de François Hollande',
+    'Flanby Hollande',
+    'Hollande le président mou',
+    "le gros Hollande", 
+    "Hollande le président moisi", 
+    "ce mauvais président qu'était François Hollande", 
+    "Hollande le mauvais président", 
+    "François Hollande le regrétté (non)",
+    "cette feignasse de François Hollande", 
+    "Hollande le minable",
+    "François Hollande ce vieux naze",
+    "cette moule de François Hollande"
+];
+
+var sarkos = [
+    "Sarko la fripouille", 
+    "le petit Nicolas Sarkozy", 
+    "cette racaille de Sarkozy",
+    "Sarko le ripou",
+    "Sarkozy le voleur",
+    "cet enfoiré de Sarkozy",
+    "cette hyène de Nicolas Sarkozy",
+    "Sarko l'enfoiré",
+    "Sarko la magouille",
+    "cet empaffé de Nicolas Sarkozy", 
+    "Sarkozy le bandit", 
+    "Sarkozy ce petit escroc", 
+    "ce pickpocket de Nicolas Sarkozy", 
+    "Sarkozy le gangster"
+];
+
+var trumps = [
+    "cet imbécile de Trump",
+    "Trump l'abruti", 
+    "cette enflure de Trump", 
+    "Donald Trump le crétin", 
+    "ce gros con de Trump", 
+    "Donald Trump le misogyne", 
+    "Trump ce gros raciste", 
+    "Trump le teubé", 
+    "Donald le gros porc", 
+    "ce FDP de Trump", 
+    "cette vieille baltringue de Trump", 
+    "Donald Trump le vieux dégueulasse", 
+    "ce sac à merde de Trump", 
+    "Trump ce gros FDP", 
+    "Trump le blondinet"
+];
+
+var valls = [
+    "Valls la fripouille", 
+    "Valls l'ordure", 
+    "Manuel Valls le vicieux",
+    "Valls le loser", 
+    "ce coquinou de Manuel Valls", 
+    "Valls la tête à claque", 
+    "ce petit enfoiré de Manuel Valls", 
+    "ce perdant de Manuel Valls"
+];
+
+var didReplace = false;
+
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function capLastOfThreeLetter(match) {
+    return match.substring(0,2) + match.charAt(2).toUpperCase();
+}
+
+function replacer(list){
+    didReplace = true;
+    return list[randomIntFromInterval(0,list.length-1)];
+}
+
+
+function walkText(node) {
+  if (node.nodeType == 3) {
+    node.data = node.data.replace(/[fF]ran[cç]ois [fF]illon/g, replacer(fillons));
+    node.data = node.data.replace(/[Pp]hilippe [pP]outou/g, replacer(poutous));
+    node.data = node.data.replace(/[jJ]ean[ -][lL]uc [mM][ée]lenchon/g, replacer(melenchons));
+    node.data = node.data.replace(/[eE]mmanuel [mM]acron/g, replacer(macrons));
+    node.data = node.data.replace(/[fF]ran[cç]ois [aA]sselineau/g, replacer(asselineaux));
+    node.data = node.data.replace(/[nN]icolas [dD]upont[- ][aA]ignan/g, replacer(aignans));
+    node.data = node.data.replace(/[jJ]ean [lL]assalle/g, replacer(lassalles));
+    node.data = node.data.replace(/[jJ]acques [cC]heminade/g, replacer(cheminades));
+    node.data = node.data.replace(/[bB]eno[iî]t [hH]amon/g, replacer(hamons));
+    node.data = node.data.replace(/[mM]arine [lL]e [pP]en/g, replacer(pens));
+    node.data = node.data.replace(/[nN]athalie [aA]rthaud/g, replacer(arthauds));
+    node.data = node.data.replace(/[fF]ran[cç]ois [hH]ollande/g, replacer(hollandes));
+    node.data = node.data.replace(/[dD]onald [tT]rump/g, replacer(arthauds));
+    node.data = node.data.replace(/[mM]anuel [vV]alls/g, replacer(valls));
+    if(didReplace){
+        node.data = node.data.replace(/\. [a-z]/g, capLastOfThreeLetter);
+        node.data = capFirstLetter(node.data);
+        didReplace = false;
+    }
+  }
+  if (node.nodeType == 1 && node.nodeName != "SCRIPT") {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      walkText(node.childNodes[i]);
+    }
+  }
+}
+
+walkText(document.body);
